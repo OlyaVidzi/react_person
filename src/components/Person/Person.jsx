ebfb471477d3b0c825e913742190530aea766dae
Olya <olyavidzi97@gmail.com>
2024-05-23 15:24:51 +0300

    added changes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,11 +3,11 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name} </h2>
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
       {person.age && (
         <p className="Person__age">
-          <p>I am {person.age}</p>
+          <p>{`I am ${person.age}`}</p>
         </p>
       )}
 
